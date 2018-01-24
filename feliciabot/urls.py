2eb94e6092fbbc4151a66003b7ffc65a02d9e8d6
Phillip B Oni <tundephillip@icloud.com>
2018-01-24 17:47:35 -0500

    New commit

---
+++ feliciabot/urls.py
@@ -0,0 +1,25 @@
+"""feliciabot URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import include, url
+from django.contrib import admin
+from chatterbot.ext.django_chatterbot import urls as chatterbot_urls
+from feliciabot.views import ChatterBotAppView
+
+urlpatterns = [
+	url(r'^api/chatterbot/', include(chatterbot_urls, namespace='chatterbot')),
+    url(r'^admin/', admin.site.urls),
+url(r'^$', ChatterBotAppView.as_view(), name='main'),
+]
